34d33435608eee7102dd52e95f807faaea8f3b2e
Gajamohan Mohanarajah <gajan@nekobasu.(none)>
2011-07-25 08:53:41 +0200

    Adding handEyeCalibration package - Gajan

---
+++ handEyeCalibration/build/CMakeFiles/handEyeCalibration.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/handEyeCalibration.dir/src/handEyeCalibration.o"
+  "../bin/handEyeCalibration.pdb"
+  "../bin/handEyeCalibration"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/handEyeCalibration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
