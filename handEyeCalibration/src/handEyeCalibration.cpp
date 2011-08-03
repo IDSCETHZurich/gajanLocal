302a084a9dd33f7ac18a05c73b784414bc965ddb
Gajamohan Mohanarajah <gajan@ethz.ch>
2011-08-03 06:53:54 +0200

    Code clean up - Gajan

--- handEyeCalibration/src/handEyeCalibration.cpp
+++ handEyeCalibration/src/handEyeCalibration.cpp
@@ -1,8 +1,8 @@
 #include "handEyeCalibration.hpp"
 
 CalibrationNode::CalibrationNode(ros::NodeHandle& n):
-ROSNode (n),
-imageTransport (n)
+	ROSNode (n),
+	imageTransport (n)
 {
 	int width, height;
 	this->ROSNode.getParam ("handEyeCalibration/width", width);
@@ -19,18 +19,19 @@ imageTransport (n)
 	this->ROSNode.getParam ("handEyeCalibration/pattern_height", pattern_height);
 	pattern_size = cv::Size_<double> (pattern_width, pattern_height);
 
-	pattern_to_base = Eigen::Vector4d::Zero ();
-	this->ROSNode.getParam ("handEyeCalibration/pattern_to_base/x", pattern_to_base(0));
-	this->ROSNode.getParam ("handEyeCalibration/pattern_to_base/y", pattern_to_base(1));
-	this->ROSNode.getParam ("handEyeCalibration/pattern_to_base/z", pattern_to_base(2));
-	this->ROSNode.getParam ("handEyeCalibration/pattern_to_base/w", pattern_to_base(3));
+//	pattern_to_base = Eigen::Vector4d::Zero ();
+//	this->ROSNode.getParam ("handEyeCalibration/pattern_to_base/x", pattern_to_base(0));
+//	this->ROSNode.getParam ("handEyeCalibration/pattern_to_base/y", pattern_to_base(1));
+//	this->ROSNode.getParam ("handEyeCalibration/pattern_to_base/z", pattern_to_base(2));
+//	this->ROSNode.getParam ("handEyeCalibration/pattern_to_base/w", pattern_to_base(3));
 
-	calibration_image_subscriber = imageTransport.subscribe("/camera/image_color", 1, &CalibrationNode::callback, this);
-
-	pose_robot_subscriber = ROSNode.subscribe ("robot_pose", 1, &CalibrationNode::storePose, this);
+	calibrationImageSubscriber = imageTransport.subscribe("/camera/image_color", 1, &CalibrationNode::imgCallback, this);
+	robotPoseSubscriber = ROSNode.subscribe ("robotPose", 1, &CalibrationNode::poseCallback, this);
 
 	cv::namedWindow (IMAGE_WINDOW, CV_WINDOW_AUTOSIZE);
-	cv::setMouseCallback (IMAGE_WINDOW, &CalibrationNode::mouseCallback, (void*)this);
+	cv::setMouseCallback (IMAGE_WINDOW, &CalibrationNode::mouseCallback, this);
+
+	readPoseFlag = false;
 }
 
 CalibrationNode::~CalibrationNode ()
@@ -38,7 +39,7 @@ CalibrationNode::~CalibrationNode ()
 	cv::destroyWindow (IMAGE_WINDOW);
 }
 
-void CalibrationNode::callback (const sensor_msgs::ImageConstPtr& msg)
+void CalibrationNode::imgCallback (const sensor_msgs::ImageConstPtr& msg)
 {
 	cv_bridge::CvImagePtr img_ptr;
 	try
@@ -55,147 +56,115 @@ void CalibrationNode::callback (const sensor_msgs::ImageConstPtr& msg)
 	cv::waitKey (10);
 }
 
-void CalibrationNode::storePose (const geometry_msgs::PoseConstPtr& msg)
+void CalibrationNode::poseCallback (const geometry_msgs::PoseConstPtr& msg)
 {
-	if (pose_robot.position.x != msg->position.x
-			|| pose_robot.position.y != msg->position.y
-			|| pose_robot.position.z != msg->position.z
-			|| pose_robot.orientation.x != msg->orientation.x
-			|| pose_robot.orientation.y != msg->orientation.y
-			|| pose_robot.orientation.z != msg->orientation.z
-			|| pose_robot.orientation.w != msg->orientation.w)
+	if (readPoseFlag)
 	{
-		pose_robot.position = msg->position;
-		pose_robot.orientation = msg->orientation;
-
-		std::cout << "New robot position: x = " << pose_robot.position.x
-				<< " y = " << pose_robot.position.y
-				<< " z = " << pose_robot.position.z << std::endl;
-		std::cout << "New robot orientation: x = " << pose_robot.orientation.x
-				<< " y = " << pose_robot.orientation.y
-				<< " z = " << pose_robot.orientation.z
-				<< " w = " << pose_robot.orientation.w << std::endl;
-
-		std::cout << pose_robot_stored << ", robot pose stored.\n";
+		std::cout << "robot POSE read" << std::endl;
+		readPoseFlag = false;
 	}
 
-	std::cout << pose_robot_not_stored << ", robot pose not stored.\n";
 }
 
-void CalibrationNode::mouseCallback (int event, int x, int y, int flags, void* param)
+void CalibrationNode::mouseCallback (int event, int x, int y, int flags, void* calibrationNode)
 {
-	CalibrationNode* Calibration = static_cast<CalibrationNode*>(param);
 
 	switch (event)
 	{
 	case CV_EVENT_LBUTTONDOWN:
-		Calibration->storeCheckboard (Calibration);
+		(static_cast<CalibrationNode*> (calibrationNode))->storeData ();
 
 		break;
 
 	case CV_EVENT_RBUTTONDOWN:
-		Calibration->calibrateCamera (Calibration);
+//		calibrateCamera ();
 
 		break;
 	}
 }
 
-int CalibrationNode::storeCheckboard (CalibrationNode* Calibration)
+int CalibrationNode::storeData ()
 {
 	cv::vector<cv::Point2f> corners;
-	bool patternWasFound = cv::findChessboardCorners (Calibration->image, Calibration->pattern, corners, cv::CALIB_CB_ADAPTIVE_THRESH + cv::CALIB_CB_NORMALIZE_IMAGE + cv::CALIB_CB_FAST_CHECK);
+	bool patternWasFound = cv::findChessboardCorners (image, pattern, corners, cv::CALIB_CB_ADAPTIVE_THRESH + cv::CALIB_CB_NORMALIZE_IMAGE + cv::CALIB_CB_FAST_CHECK);
 	if (patternWasFound)
 	{
-		geometry_msgs::Pose temp_pose;
-		temp_pose.position = Calibration->pose_robot.position;
-		temp_pose.orientation = Calibration->pose_robot.orientation;
-		Calibration->pose_robot_vector.push_back (temp_pose);
+		robotPoseVector.push_back (robotPose);
 
 		cv::Mat gray_image;
-		gray_image.create (Calibration->image_size, CV_8UC1);
-		cv::cvtColor (Calibration->image, gray_image, CV_BGR2GRAY, 0);
+		gray_image.create (image_size, CV_8UC1);
+		cv::cvtColor (image, gray_image, CV_BGR2GRAY, 0);
 
 		cv::cornerSubPix (gray_image, corners, cv::Size (5, 5), cv::Size (-1, -1),
 				cv::TermCriteria (CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
 
-		cv::drawChessboardCorners (Calibration->image, Calibration->pattern, corners, patternWasFound);
-		cv::imshow (IMAGE_WINDOW, Calibration->image);
+		cv::drawChessboardCorners (image, pattern, corners, patternWasFound);
+		cv::imshow (IMAGE_WINDOW, image);
 
-		Calibration->image_points.push_back (corners);
-
-		Calibration->getObjectPoints (Calibration);
+		image_points.push_back (corners);
 
 		std::cout << "The corner and the object points have been stored!\nPlease press a key to continue!\n";
 		cv::waitKey ();
 
-		return checkboard_found;
+		return checkerboard_found;
 	}
 	else
 	{
-		std::cout << "No checkboard has been found or it is not completely visible (or the key has not been pressed after the checkboard has been found)!\nPlease press a key to continue!\n";
+		std::cout << "No checkerboard has been found or it is not completely visible" << std::endl;
 		cv::waitKey ();
 
-		return checkboard_not_found;
+		return checkerboard_not_found;
 	}
 }
 
 
-int CalibrationNode::getObjectPoints (CalibrationNode* Calibration)
+int CalibrationNode::getObjectPoints ()
 {
 	cv::vector<cv::Point3f> temp_vector;
 
-	for (int i = 0; i < Calibration->pattern.height; i++)
+	for (int i = 0; i < pattern.height; i++)
 	{
-		for (int j = 0; j < Calibration->pattern.width; j++)
+		for (int j = 0; j < pattern.width; j++)
 		{
-			cv::Point3f temp_point = cv::Point3f (j * Calibration->pattern_size.width, i * Calibration->pattern_size.height, 0.0);
+			cv::Point3f temp_point = cv::Point3f (j * pattern_size.width, i * pattern_size.height, 0.0);
 			temp_vector.push_back (temp_point);
 		}
 	}
 
-	Calibration->object_points.push_back (temp_vector);
+	object_points.push_back (temp_vector);
 
 	return object_points_stored;
 }
 
-cv::vector<cv::vector<cv::Point2f> > CalibrationNode::getImagePoints ()
+//cv::vector<cv::vector<cv::Point2f> > CalibrationNode::getImagePoints ()
+//{
+//	return image_points;
+//}
+//cv::vector<cv::vector<cv::Point3f> > CalibrationNode::getObjectPoints ()
+//{
+//	return object_points;
+//}
+
+
+int CalibrationNode::calibrateCamera ()
 {
-	return image_points;
-}
-cv::vector<cv::vector<cv::Point3f> > CalibrationNode::getObjectPoints ()
-{
-	return object_points;
-}
+//	cv::vector<cv::Mat> rvecs, tvecs;
+//	cv::calibrateCamera (getObjectPoints (), getImagePoints (), image_size,
+//			camera_matrix, distortion_coefficients, rvecs, tvecs);
 
-
-int CalibrationNode::calibrateCamera (CalibrationNode* Calibration)
-{
-	cv::vector<cv::Mat> rvecs, tvecs;
-	cv::calibrateCamera (Calibration->getObjectPoints (), Calibration->getImagePoints (), Calibration->image_size,
-			Calibration->camera_matrix, Calibration->distortion_coefficients, rvecs, tvecs);
-
-
-
-//	cv::vector<cv::Mat> rmats;
-//	for (int i = 0; i < (int)(rvecs.size ()); i++)
-//	{
-//		cv::Mat temp_mat;
-//		cv::Rodrigues (rvecs[i], temp_mat);
-//	}
-
-	return Calibration->camera_calibrated;
+	return camera_calibrated;
 }
 
 
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "CMgreenBallDetector"); // initialize the CMgreenBallDetector node
+	ros::init(argc, argv, "handEyeCalibrationNode"); // initialize the CMgreenBallDetector node
 
 	ros::NodeHandle n; // declare a node handle
 
 	CalibrationNode CalibrationObject (n); // start the ball detector node with the node handle n
 
-	ros::spin(); // wait for callbacks until the node is destroyed
+	ros::spin();
 
 	return 0;
 }
--- handEyeCalibration/src/handEyeCalibration.hpp
+++ handEyeCalibration/src/handEyeCalibration.hpp
@@ -19,8 +19,8 @@ private:
 	{
 		pose_robot_stored 			= 0,
 		pose_robot_not_stored 		= 1,
-		checkboard_found 			= 2,
-		checkboard_not_found 		= 3,
+		checkerboard_found 			= 2,
+		checkerboard_not_found 		= 3,
 		object_points_stored 		= 4,
 		camera_calibrated			= 5,
 		camera_not_calibrated		= 6
@@ -29,11 +29,11 @@ private:
 	ros::NodeHandle ROSNode;
 
 	image_transport::ImageTransport imageTransport;
-	image_transport::Subscriber calibration_image_subscriber;
+	image_transport::Subscriber calibrationImageSubscriber;
 
-	ros::Subscriber pose_robot_subscriber;
+	ros::Subscriber robotPoseSubscriber;
 
-	geometry_msgs::Pose pose_robot;
+	geometry_msgs::Pose robotPose;
 
 	cv::Size image_size;
 	cv::Size pattern;
@@ -43,11 +43,13 @@ private:
 
 	cv::vector<cv::vector<cv::Point2f> > image_points;
 	cv::vector<cv::vector<cv::Point3f> > object_points;
-	cv::vector<geometry_msgs::Pose> pose_robot_vector;
+	cv::vector<geometry_msgs::Pose> robotPoseVector;
 
 	cv::Mat camera_matrix, distortion_coefficients;
 
-	Eigen::Vector4d pattern_to_base;
+	bool readPoseFlag;
+
+//	Eigen::Vector4d pattern_to_base;
 
 
 public:
@@ -57,22 +59,22 @@ public:
 
 	~CalibrationNode ();
 
-	void callback (const sensor_msgs::ImageConstPtr& msg);
+	void imgCallback (const sensor_msgs::ImageConstPtr& msg);
 
-	void storePose (const geometry_msgs::PoseConstPtr& msg);
+	void poseCallback (const geometry_msgs::PoseConstPtr& msg);
 
 	static void mouseCallback (int event, int x, int y, int flags, void* param);
 
-	int storeCheckboard (CalibrationNode* Calibration);
+	int storeData ();
 
-	int getObjectPoints (CalibrationNode* Calibration);
+	int getObjectPoints ();
 
-	cv::vector<cv::vector<cv::Point2f> > getImagePoints ();
+//	cv::vector<cv::vector<cv::Point2f> > getImagePoints ();
+//
+//	cv::vector<cv::vector<cv::Point3f> > getObjectPoints ();
 
-	cv::vector<cv::vector<cv::Point3f> > getObjectPoints ();
+	int calibrateCamera ();
 
-
-	int calibrateCamera (CalibrationNode* Calibration);
 };
 
 #endif /* CAMERA_CALIBRATION_HPP_ */
