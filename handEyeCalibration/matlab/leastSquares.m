92f74b54551df4af2093da68fab6b3707cbc9a6f
Gajamohan Mohanarajah <gajan@ethz.ch>
2011-08-19 12:32:09 +0200

    Matlab implementation of least squares transformation

--- handEyeCalibration/matlab/leastSquares.m
+++ handEyeCalibration/matlab/leastSquares.m
@@ -1,21 +1,36 @@
 % LS solution
 
-M = zeros(3,3);
+% import data into matlab
+dataset10
 
-for i = 1:10
-    for j=(i+1):10
-        rbi1 = eval(['rotRB',num2str(i)]);
-        rbi2 = eval(['rotRB',num2str(j)]);
+M = zeros(3,3);
+C = [];
+bA = [];
+bB = [];
+
+numberOfSamples = 0;
+
+for i = 1:9
+%    for j=(i+1):10
+        numberOfSamples = numberOfSamples + 1;
+
+        j = i+1;
+        rbi1 = [eval(['rotRB',num2str(i)])  eval(['transRB',num2str(i)]); 0 0 0 1];
+        rbi2 = [eval(['rotRB',num2str(i+1)])  eval(['transRB',num2str(j)]); 0 0 0 1];
         Bi = inv(rbi2)*rbi1;
 
-        cbi1 = eval(['rotCB',num2str(i)]);
-        cbi2 = eval(['rotCB',num2str(j)]);
+        cbi1 = [eval(['rotCB',num2str(i)])  eval(['transCB',num2str(i)]); 0 0 0 1];
+        cbi2 = [eval(['rotCB',num2str(i+1)])  eval(['transCB',num2str(j)]); 0 0 0 1];
         Ai = cbi2*inv(cbi1);
         
-        alpha = getLogTheta(Ai);
-        beta = getLogTheta(Bi);
+        alpha = getLogTheta(Ai(1:3,1:3));
+        beta = getLogTheta(Bi(1:3, 1:3));
         M = M + beta*alpha';
-    end
+        
+        C = [C; eye(3) - Ai(1:3,1:3)];
+        bA = [bA; Ai(1:3,4)];
+        bB = [bB; Bi(1:3,4)];
+%    end
 end
 
 [V,D] = eig(M'*M);
@@ -25,14 +40,7 @@ x_est = V * Lambda * inv(V) * M';
 
 display(x_est);
 
-% for i = 1:9
-%     rbi1 = eval(['rotRB',num2str(i)]);
-%     rbi2 = eval(['rotRB',num2str(i+1)]);
-%     Ai = inv(rbi2)*rbi1;
-%     
-%     cbi1 = eval(['rotCB',num2str(i)]);
-%     cbi2 = eval(['rotCB',num2str(i+1)]);
-%     Bi = cbi2*inv(cbi1);
-%     
-%     display( Ai * x_est -  x_est * Bi); 
-% end
+d = bA - kron(eye(numberOfSamples), x_est)*bB; 
+
+bX = (C'*C)\C'*d; 
+display(bX);
