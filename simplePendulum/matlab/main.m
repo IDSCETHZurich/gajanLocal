a284a066aaab7ec0fcd6e4ee7e0733c77945d89d
Francisco Ramos <francisco.ramos.f@gmail.com>
2011-11-03 18:25:10 +0100

    Adding a SIMULINK model

--- simplePendulum/matlab/main.m
+++ simplePendulum/matlab/main.m
@@ -9,18 +9,21 @@ Ac = [0 g/l 0 0;
    0 0 0 0;
    0 0 1 0];
 Bc = [-1; 0; 1; 0];
-Cc = [0 1 0 1];
-Dc = 0;
+Cc = [eye(4)];
+Dc = zeros(4,1);
 
 
-Q = diag([100,200,1,2]);
-R = 0.002;
+% Q = diag([100,200,1,2]);
+% R = 0.002;
+
+Q = diag([1,2,1,2]);
+R = 1;
 
 [Kc,~,~] = lqr(Ac, Bc, Q, R, zeros(4,1));
 display(Kc);
 
 
-%% Descretize the sub-system
+%% Discretize the sub-system
 
 %Sampling time
 dT = 1/50; % 50 Hz control
@@ -36,10 +39,10 @@ Dd = sysd.d;
 
 [Kd,~,~] = dlqr(Ad, Bd, Q, R, zeros(4,1)); 
 display(Kd);
-% x0 = [0 0.05 0 0]';
-% x = [0, x0'];
+x0 = [0 0.1 0 0]';
+x = [0, x0'];
 % for T=0:dT:4    
-%    u = - K*x0;
+%    u = - Kc*x0;
 %    [~,Y] = ode45(@pendDynamics,[0 dT],x0);
 %    [i, ~] = size(Y);
 %    x0 = Y(i,:)';
