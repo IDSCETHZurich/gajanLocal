fc3aa32952d703122bf52646488980e9e19b73bb
Mohanarajah Gajamohan <gajamohan.m@gmail.com>
2012-03-26 10:54:24 +0200

    Added sba and its dependencies

--- re_articulationViz/src/viz.cpp
+++ re_articulationViz/src/viz.cpp
@@ -1,116 +1,120 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "geometry_msgs/Pose.h"
 #include "visualization_msgs/Marker.h"
 #include "visualization_msgs/MarkerArray.h"
+#include "kdl/frames.hpp"
 
 #define PI 3.14159265
-#define RATE 10
+
+ros::Publisher vis_pub;
+
+bool publishMarkerArray(geometry_msgs::Pose axis, std::vector<geometry_msgs::Pose> trajectoyPoints, std::string refFrameName){
+	visualization_msgs::MarkerArray tmpMarkerArray;
+
+	KDL::Rotation axisR = KDL::Rotation::Quaternion(axis.orientation.x, axis.orientation.y,axis.orientation.z,axis.orientation.w);
+	KDL::Rotation deltaR = KDL::Rotation::Quaternion(0.0,std::sin(-PI/4),0.0,std::cos(-PI/4));
+	KDL::Rotation markerR = axisR*deltaR;
+
+	visualization_msgs::Marker axisMarker;
+	axisMarker.header.frame_id = refFrameName;
+	axisMarker.header.stamp = ros::Time();
+	axisMarker.ns = "articulationNS";
+	axisMarker.id = 0;
+	axisMarker.type = visualization_msgs::Marker::ARROW;
+	axisMarker.action = visualization_msgs::Marker::ADD;
+	axisMarker.scale.x = 2;
+	axisMarker.scale.y = 2;
+	axisMarker.scale.z = 2;
+	axisMarker.color.a = 1.0;
+	axisMarker.color.r = 0.0;
+	axisMarker.color.g = 1.0;
+	axisMarker.color.b = 0.0;
+
+	//std::cout << "Before: "<<axisMarker.pose.orientation.x << " " << axisMarker.pose.orientation.y << " " << axisMarker.pose.orientation.z << " " << axisMarker.pose.orientation.w << std::endl;
+	markerR.GetQuaternion(axisMarker.pose.orientation.x,axisMarker.pose.orientation.y,axisMarker.pose.orientation.z,axisMarker.pose.orientation.w);
+	//std::cout << "After: "<<axisMarker.pose.orientation.x << " " << axisMarker.pose.orientation.y << " " << axisMarker.pose.orientation.z << " " << axisMarker.pose.orientation.w << std::endl;
+
+
+	visualization_msgs::Marker axisText;
+	axisText.header.frame_id = refFrameName;
+	axisText.header.stamp = ros::Time();
+	axisText.ns = "articulationNS";
+	axisText.id = 1;
+	axisText.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
+	axisText.action = visualization_msgs::Marker::ADD;
+	axisText.scale.x = 0.5;
+	axisText.scale.y = 0.5;
+	axisText.scale.z = 0.5;
+	axisText.color.a = 1.0;
+	axisText.color.r = 1.0;
+	axisText.color.g = 1.0;
+	axisText.color.b = 1.0;
+
+	visualization_msgs::Marker trajPoint;
+	trajPoint.header.frame_id = refFrameName;
+	trajPoint.header.stamp = ros::Time();
+	trajPoint.ns = "articulationNS";
+	trajPoint.type = visualization_msgs::Marker::SPHERE;
+	trajPoint.action = visualization_msgs::Marker::ADD;
+	trajPoint.scale.x = .3;
+	trajPoint.scale.y = .3;
+	trajPoint.scale.z = .3;
+	trajPoint.color.a = 1.0;
+	trajPoint.color.r = 0.0;
+	trajPoint.color.g = 1.0;
+	trajPoint.color.b = 0.0;
+
+	axisText.pose = axis;
+	//std::stringstream s;
+	//s << "(" << axisText.pose.position.x << ", " << axisText.pose.position.y << ")" ;
+	axisText.text = "Axis of Rotation";
+
+	tmpMarkerArray.markers.push_back(axisMarker);
+	tmpMarkerArray.markers.push_back(axisText);
+
+	for(int i=0; i<(int)trajectoyPoints.size(); i++){
+		trajPoint.pose = trajectoyPoints[i];
+		trajPoint.id = 2 + i;
+		tmpMarkerArray.markers.push_back(trajPoint);
+	}
+
+	ROS_INFO("Publishing MarkerArray: Size=%d",(int)tmpMarkerArray.markers.size());
+	vis_pub.publish( tmpMarkerArray );
+
+	return true;
+}
+
 
 int main(int argc, char **argv)
 {
+  //some housekeeping
+  geometry_msgs::Pose axis;
+  axis.orientation.x = 0.0;
+  axis.orientation.y = 0.0;
+  axis.orientation.z = 0.0;
+  axis.orientation.w = 1.0;
+  std::vector<geometry_msgs::Pose> trajectoyPoints;
+
+  for(int i=0; i < 10; i++){
+	  geometry_msgs::Pose tmpPose;
+	  tmpPose.position.x = 10*std::sin(i*PI/20.);
+	  tmpPose.position.y = 10*std::cos(i*PI/20.);
+	  trajectoyPoints.push_back(tmpPose);
+  }
 
   ros::init(argc, argv, "viz");
   ros::NodeHandle _nh;
+  vis_pub = _nh.advertise<visualization_msgs::MarkerArray>( "re_visualization_marker_array", 0 );
 
-  double theta = 0.0;
+	ros::Rate loop_rate(1);
 
-  ros::Publisher vis_pub = _nh.advertise<visualization_msgs::MarkerArray>( "re_visualization_marker_array", 0 );
-
-  visualization_msgs::Marker marker1;
-  marker1.header.frame_id = "/arm_0_link";
-  marker1.header.stamp = ros::Time();
-  marker1.ns = "articulationNS";
-  marker1.id = 0;
-  marker1.type = visualization_msgs::Marker::SPHERE;
-  marker1.action = visualization_msgs::Marker::ADD;
-  //marker1.pose.position.x = 1;
-  //marker1.pose.position.y = 1;
-  marker1.pose.position.z = 1;
-  marker1.pose.orientation.x = 0.0;
-  marker1.pose.orientation.y = 0.0;
-  marker1.pose.orientation.z = 0.0;
-  marker1.pose.orientation.w = 1.0;
-  marker1.scale.x = .3;
-  marker1.scale.y = .3;
-  marker1.scale.z = .3;
-  marker1.color.a = 1.0;
-  marker1.color.r = 0.0;
-  marker1.color.g = 1.0;
-  marker1.color.b = 0.0;
-
-  visualization_msgs::Marker marker2;
-  marker2.header.frame_id = "/arm_0_link";
-  marker2.header.stamp = ros::Time();
-  marker2.ns = "articulationNS";
-  marker2.id = 1;
-  marker2.type = visualization_msgs::Marker::ARROW;
-  marker2.action = visualization_msgs::Marker::ADD;
-  marker2.pose.position.x = 0;
-  marker2.pose.position.y = 0;
-  marker2.pose.position.z = 0;
-  marker2.pose.orientation.x = 0.0;
-  marker2.pose.orientation.y = -1./std::sqrt(2);
-  marker2.pose.orientation.z = 0.0;
-  marker2.pose.orientation.w = 1./std::sqrt(2);
-  marker2.scale.x = 2;
-  marker2.scale.y = 2;
-  marker2.scale.z = 2;
-  marker2.color.a = 1.0;
-  marker2.color.r = 0.0;
-  marker2.color.g = 1.0;
-  marker2.color.b = 0.0;
-
-  visualization_msgs::Marker marker3;
-  marker3.header.frame_id = "/arm_0_link";
-  marker3.header.stamp = ros::Time();
-  marker3.ns = "articulationNS";
-  marker3.id = 2;
-  marker3.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
-  marker3.action = visualization_msgs::Marker::ADD;
-  marker3.pose.position.x = 0;
-  marker3.pose.position.y = 0;
-  marker3.pose.position.z = 1;
-  marker3.pose.orientation.x = 0.0;
-  marker3.pose.orientation.y = 0.0;
-  marker3.pose.orientation.z = 0.0;
-  marker3.pose.orientation.w = 1.0;
-  marker3.scale.x = 0.5;
-  marker3.scale.y = 0.5;
-  marker3.scale.z = 0.5;
-  marker3.color.a = 1.0;
-  marker3.color.r = 1.0;
-  marker3.color.g = 1.0;
-  marker3.color.b = 1.0;
-
-
-  ros::Rate loop_rate(RATE);
-
-  while (ros::ok())
-  {
-    marker1.pose.position.x = 1*std::sin(theta);
-    marker1.pose.position.y = 1*std::cos(theta);
-
-    marker3.pose.position.x = marker1.pose.position.x;
-    marker3.pose.position.y = marker1.pose.position.y;
-
-    std::stringstream s;
-    s << "(" << marker3.pose.position.x << ", " << marker3.pose.position.y << ")" ;
-    marker3.text = s.str();
-
-    visualization_msgs::MarkerArray ma;
-    ma.markers.push_back(marker1);
-    ma.markers.push_back(marker2);
-    ma.markers.push_back(marker3);
-
-    //ROS_INFO("Publishing marker1");
-    vis_pub.publish( ma );
-
-    ros::spinOnce();
-    loop_rate.sleep();
-
-    theta = theta + PI / (RATE*2);
-  }
-
+	while (ros::ok())
+	{
+		publishMarkerArray(axis, trajectoyPoints, "/arm_0_link");
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
 
   return 0;
 }
