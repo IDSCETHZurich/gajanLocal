c0623122afdf7302c8cabc239dcbd9439bf0a518
Mohanarajah Gajamohan <gajan@ethz.ch>
2012-05-18 16:50:52 +0200

    some modifications to roombaEarth

--- roombaEarth/nodes/odom.m
+++ roombaEarth/nodes/odom.m
@@ -1,18 +1,23 @@
+clc;
 % odometry 
+
 
 % robot states w.r.t inertial frame
 robot_x_i = 0.0;
 robot_y_i = 0.0;
 robot_theta_i = 0.0;
+distance = 0.0;
 
 robotState = [];
-robot_description_r = 0.25 / 2.0;
+robot_description_r = 0.268 / 2.0;
+
 
 for i=1:size(data,1)
     d_dist = data(i,1)/1000.0;
     d_angle = data(i,2)/1000.0;
     
-    robot_theta_i = robot_theta_i + 2*d_angle/robot_description_r;
+    distance = distance + d_dist;
+    robot_theta_i = robot_theta_i + d_angle/robot_description_r;
     robot_x_i = robot_x_i + d_dist * cos(robot_theta_i);
     robot_y_i = robot_y_i + d_dist * sin(robot_theta_i);	
     
@@ -25,5 +30,8 @@ for i=1:size(data,1)
         robot_x_i, robot_y_i, robot_theta_i, obstacle_x, obstacle_y, range];
 end
 
-plot(robotState(:,4), robotState(:,5), 'x',robotState(:,1), robotState(:,2), 'r')
+%plot(robotState(:,4), robotState(:,5),'xb',robotState(:,1), robotState(:,2), '-r')
+plot(robotState(:,1), robotState(:,2), '-r')
 axis equal
+
+display(distance)
